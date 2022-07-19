bc2c268db08c77ff7872db1f3250c85e5814c150
Rob Caelers <rob.caelers@gmail.com>
2022-07-19 19:36:40 +0200

    Add StringUtils tests

---
+++ libs/utils/test/UnfoldTestUtils.cc
@@ -0,0 +1,117 @@
+// Copyright (C) 2022 Rob Caelers <rob.caelers@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <boost/test/tools/old/interface.hpp>
+#include <memory>
+#include <fstream>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#if SPDLOG_VERSION >= 10600
+#  include <spdlog/pattern_formatter.h>
+#endif
+#if SPDLOG_VERSION >= 10801
+#  include <spdlog/cfg/env.h>
+#endif
+
+#include "utils/Logging.hh"
+#include "utils/StringUtils.hh"
+
+#define BOOST_TEST_MODULE "unfold"
+#include <boost/test/unit_test.hpp>
+
+using namespace unfold::utils;
+
+struct GlobalFixture
+{
+  GlobalFixture() = default;
+  ~GlobalFixture() = default;
+
+  GlobalFixture(const GlobalFixture &) = delete;
+  GlobalFixture &operator=(const GlobalFixture &) = delete;
+  GlobalFixture(GlobalFixture &&) = delete;
+  GlobalFixture &operator=(GlobalFixture &&) = delete;
+
+  void setup()
+  {
+    const auto *log_file = "unfold-test-utils.log";
+
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_file, false);
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+
+    auto logger{std::make_shared<spdlog::logger>("unfold", std::initializer_list<spdlog::sink_ptr>{file_sink, console_sink})};
+    logger->flush_on(spdlog::level::critical);
+    spdlog::set_default_logger(logger);
+
+    spdlog::set_level(spdlog::level::info);
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%-5l%$] %v");
+
+#if SPDLOG_VERSION >= 10801
+    spdlog::cfg::load_env_levels();
+#endif
+  }
+
+private:
+};
+
+struct Fixture
+{
+  Fixture() = default;
+  ~Fixture() = default;
+
+  Fixture(const Fixture &) = delete;
+  Fixture &operator=(const Fixture &) = delete;
+  Fixture(Fixture &&) = delete;
+  Fixture &operator=(Fixture &&) = delete;
+
+private:
+  std::shared_ptr<spdlog::logger> logger{Logging::create("test")};
+};
+
+BOOST_TEST_GLOBAL_FIXTURE(GlobalFixture);
+
+BOOST_FIXTURE_TEST_SUITE(unfold_test, Fixture)
+
+BOOST_AUTO_TEST_CASE(utils_string_utf16_to_utf8)
+{
+  std::string s = unfold::utils::utf16_to_utf8(L"Hello World");
+  BOOST_CHECK_EQUAL(s, "Hello World");
+}
+
+BOOST_AUTO_TEST_CASE(utils_string_utf8_to_utf16)
+{
+  std::wstring s = unfold::utils::utf8_to_utf16("Hello World");
+  BOOST_CHECK(s == L"Hello World");
+}
+
+BOOST_AUTO_TEST_CASE(utils_empty_string_utf16_to_utf8)
+{
+  std::string s = unfold::utils::utf16_to_utf8(L"");
+  BOOST_CHECK_EQUAL(s, "");
+}
+
+BOOST_AUTO_TEST_CASE(utils_empty_string_utf8_to_utf16)
+{
+  std::wstring s = unfold::utils::utf8_to_utf16("");
+  BOOST_CHECK(s == L"");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
